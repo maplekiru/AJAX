6236c8a85e0344754b039c87544bb148b6557810
Kiru <kirushanth.ravindran@gmail.com>
2021-04-13 17:00:32 -0700

    update basic html and get GIF

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Let's get this party started!");
+
+async function getGIF() {
+    console.log("getGIF ran")
+    let searchValue = $("#search").val();
+
+    let key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search",
+    {params: {q: searchValue, api_key: key}});
+
+    let gif = response.data.data[0].embed_url;
+
+    console.log(gif);
+}
+
+$("#submit").on("submit", getGIF);
