ee8327d6a8cd319350d17250c7b4424880912547
Sam Merham <samehisaac@yahoo.com>
2021-04-13 17:38:10 -0700

    completed second step adding gif

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 console.log("Let's get this party started!");
-
+const KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 async function getGIF() {
     console.log("getGIF ran")
     let searchValue = $("#search").val();
 
-    let key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
-
     let response = await axios.get("http://api.giphy.com/v1/gifs/search",
-    {params: {q: searchValue, api_key: key}});
+    {params: {q: searchValue, api_key: KEY}});
 
-    let gif = response.data.data[0].embed_url;
+    let gifUrl = response.data.data[0].images.original.url;
+    // let gif = response.data.data[0].url;
 
-    console.log(gif);
+    console.log(gifUrl);
+    return gifUrl;
 }
 
-$("#submit").on("submit", getGIF);
+
+ function appendGif(gif){
+    let $container = $('#container');
+    $container.append($(`<img src="${gif}">`));
+}
+
+$("#form").on("submit", async function(e){
+    e.preventDefault();
+    let gifUrl = await getGIF();
+    appendGif(gifUrl);
+});
